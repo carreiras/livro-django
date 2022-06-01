89dd00cfda1def327cbaee7b381c629a46fb1ca3
Ewerton Carreira <ewertoncarreira@gmail.com>
2022-06-01 19:12:11 -0300

    Criação do ambiente de desenvolvimento

---
+++ medicSearchAdmin/settings/production.py
@@ -0,0 +1,17 @@
+import os
+from .settings import *
+
+DEBUG = False
+
+# Crie a secret key para seu ambiente de produção
+SECRET_KEY = 'ixb6fha#ts=&b4t2u%p1_62-!8dw2j==j)d^3-j$!z(@*m+-h'
+
+# Alterar para o ip do ambiente de produção quando houver.
+ALLOWED_HOSTS = ['127.0.0.1']
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
